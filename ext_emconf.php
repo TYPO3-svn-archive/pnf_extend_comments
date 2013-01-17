415fcb86136b9f8173401d56c9028ea7db86e5f8
micpaillard <micpaillard@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-17 15:11:28 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pnf_extend_comments/trunk@70132 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "pnf_extend_comments".
  *
- * Auto generated 17-01-2013 12:59
+ * Auto generated 17-01-2013 16:09
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"c78b";s:37:"class.user_pnfextendcomments_hook.php";s:4:"c93f";s:28:"class.ux_tx_comments_pi1.php";s:4:"54b8";s:16:"ext_autoload.php";s:4:"f018";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"0f5c";s:14:"ext_tables.php";s:4:"d9c6";s:14:"ext_tables.sql";s:4:"cb41";s:16:"locallang_db.xml";s:4:"f7a6";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"076e";s:20:"doc/wizard_form.html";s:4:"797d";s:50:"scheduler/class.tx_pnfcommentsscheduler_helper.php";s:4:"b38f";s:53:"scheduler/class.tx_pnfcommentsscheduler_scheduler.php";s:4:"7bec";s:23:"scheduler/locallang.xml";s:4:"5da1";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"c78b";s:37:"class.user_pnfextendcomments_hook.php";s:4:"c93f";s:28:"class.ux_tx_comments_pi1.php";s:4:"54b8";s:16:"ext_autoload.php";s:4:"8f62";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"7238";s:14:"ext_tables.php";s:4:"d9c6";s:14:"ext_tables.sql";s:4:"cb41";s:16:"locallang_db.xml";s:4:"f7a6";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"076e";s:20:"doc/wizard_form.html";s:4:"797d";s:50:"scheduler/class.tx_pnfcommentsscheduler_helper.php";s:4:"b38f";s:53:"scheduler/class.tx_pnfcommentsscheduler_scheduler.php";s:4:"7bec";s:47:"scheduler/class.tx_pnfextendcomments_helper.php";s:4:"1e60";s:50:"scheduler/class.tx_pnfextendcomments_scheduler.php";s:4:"04e2";s:23:"scheduler/locallang.xml";s:4:"5da1";}',
 	'suggests' => array(
 	),
 );
